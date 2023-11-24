57e3ff3d26bcaf674b2fe817f15de7d2dd7d3aff
steveinit <steve@steveinit.com>
2023-11-24 15:07:14 -0500

    documented and refined inputs

--- calculate_gridCAGR.py
+++ calculate_gridCAGR.py
@@ -1,29 +1,34 @@
+######################################
+    # CAUTION: THESE ARE ESTIMATES. Dont come after me if your experience does not match the results of this logic.
+    # I've been building small solar projects for ten years, but Im not a solar install pro.
+    # I have been calculating cost:benefit for smaller projects for a while though.
+######################################
+    # Solar System vs Utility Cost Comparison
 
-"""
-Solar System vs Utility Cost Comparison
+    # This Python script calculates the Compound Annual Growth Rate (CAGR) and estimates the future cost per kWh
+    # to compare the cumulative cost of buying power from a utility company versus investing in a solar system.
 
-This Python script calculates the Compound Annual Growth Rate (CAGR) and estimates the future cost per kWh
-to compare the cumulative cost of buying power from a utility company versus investing in a solar system.
+    # Usage:
+    # 1. Adjust the 'start_year_cost', 'end_year_cost', and 'investment_years' variables as needed.
+    # 2. Run the script to obtain the CAGR and future cost per kWh.
+######################################
+    # Parameters:
+    # - start_value (float): Initial value.
+    # 
+    # - end_value (float): Final value.
+    # - num_years (int): Number of years.
 
-Usage:
-1. Adjust the 'start_year_cost', 'end_year_cost', and 'investment_years' variables as needed.
-2. Run the script to obtain the CAGR and future cost per kWh.
+    # Returns:
+    # - float: CAGR.
+######################################
+    # Notes:
+    # I'm moving this up into Lambda so I can put a frontend on it, but I havent converted this part yet. Probably should have done it first.
+######################################
 
-"""
+from decimal import *
 
 # Function to calculate Compound Annual Growth Rate (CAGR)
 def calculate_cagr(start_value, end_value, num_years):
-    """
-    Calculate CAGR (Compound Annual Growth Rate).
-
-    Parameters:
-    - start_value (float): Initial value.
-    - end_value (float): Final value.
-    - num_years (int): Number of years.
-
-    Returns:
-    - float: CAGR.
-    """
     return (end_value / start_value) ** (1 / num_years) - 1
 
 # Function to calculate future cost per kWh for intermediate years
@@ -42,16 +47,23 @@ def calculate_future_cost(start_value, cagr, num_years):
     return start_value * (1 + cagr) ** num_years
 
 # Example input values
+start_year = 1996
 start_year_cost = 0.643  # Cost per kWh in the start year
+end_year = 2020
 end_year_cost = 0.12    # Cost per kWh in the end year
-investment_years = 26   # Number of years for the comparison
+cagr_timespan = end_year - start_year
+investment_years = 20   # Number of years for the comparison
 
 # Calculate CAGR
 cagr = calculate_cagr(start_year_cost, end_year_cost, investment_years)
+converted_cagr = cagr *-100
+print(converted_cagr)
+projected_kWhCost_increase = Decimal(converted_cagr)
+print(projected_kWhCost_increase)
 
 # Calculate future cost per kWh for intermediate years
-future_cost = calculate_future_cost(start_year_cost, cagr, investment_years)
+future_cost = calculate_future_cost(end_year_cost, cagr, investment_years)
 
 # Output results
-print(f"CAGR: {cagr:.4%}")
+print(f"CAGR: {cagr:.1%}. Based on inputted cost/kWh trend over {cagr_timespan} years from {start_year}-{end_year}, \n cost per kWh will likely go up about {projected_kWhCost_increase:.2}% per year if the trend continues.")
 print(f"Future cost per kWh in {investment_years} years: ${future_cost:.4f}")
--- calculate_solarPayback.py
+++ calculate_solarPayback.py
@@ -1,19 +1,36 @@
+######################################
+    # CAUTION: THESE ARE ESTIMATES. Dont come after me if your experience does not match the results of this logic.
+    # I've been building small solar projects for ten years, but Im not a solar install pro.
+    # I have been calculating cost:benefit for smaller projects for a while though.
+######################################
+    # Calculate the payback period, total savings, estimated future monthly power bill,
+    # and total cost of staying on the grid over a specified number of years.
+    # It doesnt matter if it's just PV Panels, or a full hybrid kit. 
+    # This just compares the cost of a setup to the projected cost (CAGR) of staying 100% on grid.
+######################################
+    # Parameters:
+    # - cagr (float): Compound Annual Growth Rate (represents annual increase in grid electricity costs).
+    # --- get your CAGR from calculate_gridCAGR.py
+    # --- enter it below as a positive decimal.  I know it's negative from the script, but trust me. If you dont trust me, look up how CAGR works.
+    # --- ex: if CAGR from calculate_gridCAGR.py is -6.1%, 
+    # - initial_cost_solar_kit (float): Initial cost of the solar kit.
+    # - monthly_power_bill (float): Average monthly power bill from the grid.
+    # - num_years (int): Number of years for the calculation.
+    # --- 20-25 years is a good estimate for the lifetime of a solar kit.
+    # --- if your kit would include batteries with lifetimes shorter than your num_years, you need the cost of replacing them to initial_cost_solar_kit
+    # ----- I'll add maintenance cost into the assessment one day soon, but today is not that day.
+######################################
+    # Returns:
+    # - tuple (dont @ me gg): Payback period in months, total savings, estimated future monthly power bill,
+    #          total cost of staying on the grid over the specified number of years.
+######################################
+    # Notes:
+    # I'm moving this up into Lambda so I can put a frontend on this. Feel free to yoink that code from this repo.
+    # --- lambda_calculate_PeriodPaybackAndCosts.py
+######################################
+
 def calculate_payback_period_and_costs(cagr, initial_cost_solar_kit, monthly_power_bill, num_years):
-    """
-    Calculate the payback period, total savings, estimated future monthly power bill,
-    and total cost of staying on the grid over a specified number of years.
-    Try to be as accurate as possible pricing a solar kit that will completely offset your bill when averaged.
 
-    Parameters:
-    - cagr (float): Compound Annual Growth Rate (represents annual increase in grid electricity costs).
-    - initial_cost_solar_kit (float): Initial cost of the solar kit.
-    - monthly_power_bill (float): Average monthly power bill from the grid.
-    - num_years (int): Number of years for the calculation.
-
-    Returns:
-    - tuple: Payback period in months, total savings, estimated future monthly power bill,
-             total cost of staying on the grid over the specified number of years.
-    """
     # Estimate annual savings or earnings from solar kit
     annual_savings_earnings = cagr * initial_cost_solar_kit
 
@@ -34,11 +51,11 @@ def calculate_payback_period_and_costs(cagr, initial_cost_solar_kit, monthly_pow
 
     return payback_period_months, total_savings, future_monthly_power_bill, total_cost_on_grid
 
-# Example input values
-cagr = 0.06  # Example CAGR (6% annual increase in grid electricity costs)
-initial_cost_solar_kit = 23000.0  # Example initial cost of the solar kit in dollars
-monthly_power_bill = 350.0  # Example average monthly power bill from the grid in dollars
-num_years = 25  # Number of years for the calculation, 20-25 is a good estimate for the lifetime of a solar kit.
+# input values - change these to your parameters
+cagr = 0.06  # Example CAGR (6% annual increase in grid electricity costs is pretty normal)
+initial_cost_solar_kit = 23000.00  # Example initial cost of the solar kit in dollars
+monthly_power_bill = 350.00  # Example average monthly power bill from the grid in dollars
+num_years = 25  # Number of years for the calculation
 
 # Calculate payback period, total savings, estimated future monthly power bill, and total cost of staying on the grid
 payback_period_months, total_savings, future_monthly_power_bill, total_cost_on_grid = calculate_payback_period_and_costs(
